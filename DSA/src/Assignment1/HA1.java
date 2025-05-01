137173bad1c0e00fd268e76e734ab1b90655672d
artes26 <ayush16r@gmail.com>
2025-05-01 23:25:27 +0530

    try

--- DSA/src/Assignment1/HA1.java
+++ DSA/src/Assignment1/HA1.java
@@ -2,6 +2,8 @@ package Assignment1;
 import java.util.*;
 public class HA1 {
 	public static void main(String[] args) {
+
+		System.out.println("hello");
 		// TODO Auto-generated method stub
 		 Scanner input = new Scanner(System.in); 
 		 
